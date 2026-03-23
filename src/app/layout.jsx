611549d24a26b5231edfc8d2e7f337c6a25d7e98
Clifford <cliffordchristophermarx@gmail.com>
2026-03-23 10:18:02 +0530

    Meta Pixel addition

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,6 +9,7 @@ import { FaWhatsapp } from "react-icons/fa6";
 import CartSyncProvider from "../components/CartSyncProvider";
 import UtmVisitTracker from "../components/UtmVisitTracker";
 import CouponCapture from "../components/CouponCapture";
+import Script from "next/script";
 
 export const metadata = {
   title: {
@@ -42,6 +43,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2321708075014279');
+          fbq('track', 'PageView');
+        `}</Script>
+        <noscript>
+          <img height="1" width="1" style={{display:"none"}}
+            src="https://www.facebook.com/tr?id=2321708075014279&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body>
         <ScrollToTop />
         <Navbar />
